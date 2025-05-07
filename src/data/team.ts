e3bbba1130d03e04ab4aeac28942e7de054960fa
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2025-05-08 00:52:43 +0800

    created team.ts to contain the profile information data array

---
+++ src/data/team.ts
@@ -0,0 +1,62 @@
+export const profiles = [
+    {
+      name: "John Manuel Carado",
+      title: "Developer",
+      description:
+        "He made luto canton for the most part but still worked it out. He is expert in lutoing canton but I dunno how he pulled it off thoo..",
+      links: {
+        github: "https://github.com/hydraadra112",
+        linkedin: "https://www.linkedin.com/in/mannsmanuel/",
+        instagram: "https://www.facebook.com/hydrawitshs",
+      },
+      portfolio: "https://portfolio-mocha-rho-65.vercel.app/",
+    },
+     {
+      name: "Ethan Gabriel Soncio",
+      title: "Developer",
+      description:
+        "He made luto canton for the most part but still worked it out. He is expert in lutoing canton but I dunno how he pulled it off thoo..",
+      links: {
+        github: "https://github.com/Tzetha",
+        linkedin: "https://www.linkedin.com/in/ethan-soncio-260816194/",
+        instagram: "https://www.instagram.com/lmntriiixx/",
+      },
+      portfolio: "https://example.com/kanton-portfolio",
+    },
+     {
+      name: "Mark Andrei Encanto",
+      title: "Kanton Guy",
+      description:
+        "He made luto canton for the most part but still worked it out. He is expert in lutoing canton but I dunno how he pulled it off thoo..",
+      links: {
+        github: "https://github.com/Markndrei",
+        linkedin: "https://www.linkedin.com/in/mark-andrei-encanto-962179351/",
+        instagram: "https://www.instagram.com/markndrei/",
+      },
+      portfolio: "https://example.com/kanton-portfolio",
+    },
+     {
+      name: "Chariz Dianne Falco",
+      title: "Developer",
+      description:
+        "He made luto canton for the most part but still worked it out. He is expert in lutoing canton but I dunno how he pulled it off thoo..",
+      links: {
+        github: "https://github.com/chariz1101",
+        linkedin: "https://www.linkedin.com/in/chariz-dianne-falco-12350626a",
+        instagram: "https://www.instagram.com/cha.dianne",
+      },
+      portfolio: "https://example.com/kanton-portfolio",
+    },
+     {
+      name: "Cherilyn Marie Deocampo",
+      title: "Developer",
+      description:
+        "He made luto canton for the most part but still worked it out. He is expert in lutoing canton but I dunno how he pulled it off thoo..",
+      links: {
+        github: "https://github.com/chiichann",
+        linkedin: "https://www.linkedin.com/in/cherilyn-marie-deocampo-464938337",
+        instagram: "https://www.instagram.com/_cherxlynn/",
+      },
+      portfolio: "https://example.com/kanton-portfolio",
+    },
+  ];
